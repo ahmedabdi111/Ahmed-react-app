e63df85973ad461a296e972f3a0382bf6d64ea89
Ahmed Ali <ahmedabdirashed11.gmail.com>
2025-05-15 15:24:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// App.jsx or App.js
+import './App.css';
+import Greeting from './components/Greeting';
+import Header from './components/header';
+
+function App() {
+  return (
+    <>
+      <Greeting name="Ahmed"surname="Ali"/>
+      <Header />
+      <Header />
+      <Header />
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// components/Header.jsx
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>This is my first React tutorial</h1>
+    </div>
+  );
+};
+
+export default Header;
