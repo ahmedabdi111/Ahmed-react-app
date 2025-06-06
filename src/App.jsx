46677363fe60bd547a1b227b72ec61c2e44a1d56
ahmed abdi ali <ahmedabdirashed11@gmail.com>
2025-06-06 12:33:20 +0100

    first react

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,18 @@ import Inputer from "./components/Inputer";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Dashboard from "./components/Dashboard";
 import Notification from "./components/Notification";
+import UserList from "./components/UserList";
+import NameForm from "./components/Nameform";
+import UncontrolledInput from "./components/UncontrolledInput"
 
 function App() {
   return (
     <>
+      <NameForm />
       <ThemeToggle />
+      <UncontrolledInput />
       <Header />
+      <UserList />
       <Notification hasNewMessages = {true} />
       <Dashboard isLoggedIn={true} />
       <Inputer />
---
+++ src/components/UserList.jsx
@@ -0,0 +1,13 @@
+function UserList() {
+  const users = ['Alice', 'Bob', 'Charlie'];
+
+  return (
+    <ul>
+      {users.map((user, index) => (
+        <li key={index}>{user}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default UserList;
